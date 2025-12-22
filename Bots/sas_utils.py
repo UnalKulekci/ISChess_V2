350999a3fea382f660a677da9d55ca2aa07064b6
UnalKulekci <unlklkc@gmail.com>
2025-12-22 01:00:33 +0100

    editing comment line

--- Bots/sas_utils.py
+++ Bots/sas_utils.py
@@ -1,31 +1,22 @@
+# To check if a square is empty
 def is_square_empty(piece):
-    """
-    Bir karenin boş olup olmadığını kontrol eder.
-    None, boş string '' veya 'X' boş kabul edilir.
-    """
-    return piece in (None, '', 'X')
+      return piece in (None, '', 'X', '--')
 
+# To check if a piece is friendly
 def is_friendly_piece(piece, my_color):
-    """
-    Verilen taşın (piece) benim rengimde (my_color) olup olmadığını kontrol eder.
-    """
+ 
     if is_square_empty(piece): 
         return False
     
-    # Eğer taş bir nesne (Object) ise .color özelliğine bak
     if hasattr(piece, "color"):
         p_color = piece.color
     else:
-        # Eğer taş bir string ise (örn: "wp"), son karaktere bak ('w' veya 'b')
         p_color = piece[-1]
         
     return p_color == my_color
 
+# To check if a piece is enemy
 def is_enemy_piece(piece, my_color):
-    """
-    Verilen taşın rakip taş olup olmadığını kontrol eder.
-    (Boş değilse ve dost değilse, rakiptir).
-    """
     if is_square_empty(piece): 
         return False
     return not is_friendly_piece(piece, my_color)
